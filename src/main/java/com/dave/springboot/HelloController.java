53335729eb1ac44b4013ee97dd296f19bdba4073
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-01-24 16:19:07 +0700

    Hello Spring boot

---
+++ src/main/java/com/dave/springboot/HelloController.java
@@ -0,0 +1,12 @@
+package com.dave.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello Spring Boot";
+    }
+}
