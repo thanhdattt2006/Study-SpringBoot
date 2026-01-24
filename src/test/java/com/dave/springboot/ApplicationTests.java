306ee06bbefd4eae33ca7f05c1fd00d0dcbd36f3
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-01-24 16:05:40 +0700

    Initial push

---
+++ src/test/java/com/dave/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dave.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
