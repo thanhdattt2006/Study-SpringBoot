22c954f746967eea1a9ca6388c81102b8290dec4
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-04-02 23:42:16 +0700

    spring security, postgres

---
+++ src/main/java/com/dave/springboot/User/UserRepo.java
@@ -0,0 +1,9 @@
+package com.dave.springboot.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/dave/springboot/config/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.dave.springboot.config;
+
+import com.dave.springboot.User.User;
+import com.dave.springboot.User.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User 404 cmnr");
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
