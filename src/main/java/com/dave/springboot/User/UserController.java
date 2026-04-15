a1423e6c93c27ffb0ba9869d008b1d83fe40c89a
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-04-15 23:50:00 +0700

    fix UserController

--- src/main/java/com/dave/springboot/User/UserController.java
+++ src/main/java/com/dave/springboot/User/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
+
     @Autowired
     private UserService service;
 
